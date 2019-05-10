e89f8c4a9e47bba8bd9cae2d33e4340722b26497
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 01:01:41 +0200

    Interface used by every render-able entity

---
+++ RenderData/IRenderable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.RenderData
+{
+    interface IRenderable
+    {
+        void Render();
+    }
+}
