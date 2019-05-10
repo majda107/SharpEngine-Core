e1700193cdf3fd095bef98eedfd257c01bd574b6
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 00:57:53 +0200

    Entity interface for every entity in game

---
+++ EntityData/IEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK;
+
+namespace SharpEngine_Core.EntityData
+{
+    interface IEntity
+    {
+        /// <summary>
+        /// Position of entity
+        /// </summary>
+        public Vector3 Pos { get; set; }
+
+        /// <summary>
+        /// Roll angle of entity
+        /// </summary>
+        public float Roll { get; set; }
+
+        /// <summary>
+        /// Pitch angle of entity
+        /// </summary>
+        public float Pitch { get; set; }
+
+        /// <summary>
+        /// Yaw angle of entity
+        /// </summary>
+        public float Yaw { get; set; }
+    }
+}
