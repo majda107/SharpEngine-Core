0d646450fb29de5c8afeb0ebbb409a0f6eb113b7
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 01:02:33 +0200

    Class holding data about every material

---
+++ SolidData/Material.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+
+namespace SharpEngine_Core.SolidData
+{
+    class Material
+    {
+        public string name { get; private set; }
+
+        public float[] Ka; // ambient
+        public float[] Kd; // diffuse
+        public float[] Ks; // specular
+        public float d;
+        public float Tr;
+        public float s;
+
+        public int textureID;
+
+        public Material(string name)
+        {
+            this.name = name;
+
+            this.Ka = new float[] { 0.2f, 0.2f, 0.2f };
+            this.Kd = new float[] { 0.8f, 0.8f, 0.8f };
+            this.Ks = new float[] { 1.0f, 1.0f, 1.0f };
+
+            this.d = 1.0f;
+            this.Tr = 0;
+            this.s = 0;
+
+            this.textureID = 0;
+        }
+        public void Load()
+        {
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Ambient, Ka);
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Diffuse, Kd);
+            GL.Material(MaterialFace.FrontAndBack, MaterialParameter.Specular, Ks);
+        }
+    }
+}
