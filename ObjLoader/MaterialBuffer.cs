3b9b4a08fda04b8782fb547e5100cd6a6e77921b
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 01:00:27 +0200

    Object used by ObjLoader and MtlLoader

---
+++ ObjLoader/MaterialBuffer.cs
@@ -0,0 +1,21 @@
+﻿using SharpEngine_Core.SolidData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.ObjLoader
+{
+    class MaterialBuffer
+    {
+        public List<Material> materials { get; set; }
+        public MaterialBuffer()
+        {
+            this.materials = new List<Material>();
+        }
+
+        public void Add(Material mat)
+        {
+            this.materials.Add(mat);
+        }
+    }
+}
---
+++ ObjLoader/VertexBuffer.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.ObjLoader
+{
+    class VertexBuffer
+    {
+        public List<Vector3> vertices;
+        public List<Vector2> textures;
+        public List<Vector3> normals;
+        public VertexBuffer()
+        {
+            this.vertices = new List<Vector3>();
+            this.textures = new List<Vector2>();
+            this.normals = new List<Vector3>();
+        }
+    }
+}
