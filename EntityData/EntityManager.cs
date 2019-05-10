e814d6eb1ff9370661fc752e5755ef2c39798a3f
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 00:58:15 +0200

    Entity manager created

---
+++ EntityData/EntityManager.cs
@@ -0,0 +1,32 @@
+﻿using SharpEngine_Core.RenderData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.EntityData
+{
+    class EntityManager
+    {
+        public List<IEntity> Entities { get; private set; }
+        public EntityManager()
+        {
+            this.Entities = new List<IEntity>();
+        }
+
+        public void Add(IEntity entity)
+        {
+            this.Entities.Add(entity);
+        }
+
+        public void RenderVisible()
+        {
+            foreach(IEntity entity in this.Entities)
+            {
+                if(entity is IRenderable)
+                {
+                    (entity as IRenderable).Render();
+                }
+            }
+        }
+    }
+}
