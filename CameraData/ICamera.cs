472c16253fc8af9546139051ad2c0f6b6aa0a53f
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 00:55:25 +0200

    Created camera interface

---
+++ CameraData/ICamera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpEngine_Core.EntityData;
+using OpenTK;
+
+namespace SharpEngine_Core.CameraData
+{
+    interface ICamera : IEntity
+    {
+        /// <summary>
+        /// LookAt matrix of camera
+        /// </summary>
+        public Matrix4 LookAt { get; }
+
+        void LoadMatrix();
+        void BuildMatrix();
+    }
+}
