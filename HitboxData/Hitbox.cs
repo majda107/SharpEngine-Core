aa41d937640e3fc69f84936e92587cc9f620b006
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 00:59:32 +0200

    Actual hitbox class

---
+++ HitboxData/Hitbox.cs
@@ -0,0 +1,40 @@
+﻿using OpenTK;
+using SharpEngine_Core.SolidData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.HitboxData
+{
+    class Hitbox : AHitbox
+    {
+        public Hitbox(Face3[] faces)
+        {
+            if(faces != null)
+            {
+                float left = faces[0].vertices[0].X;
+                float right = faces[0].vertices[0].X;
+                float top = faces[0].vertices[0].Y;
+                float bottom = faces[0].vertices[0].Y;
+                float close = faces[0].vertices[0].Z;
+                float distant = faces[0].vertices[0].Z;
+
+                foreach (Face3 face in faces)
+                {
+                    foreach (Vector3 vertex in face.vertices)
+                    {
+                        if (vertex.X > left) left = vertex.X;
+                        if (vertex.X < right) right = vertex.X;
+                        if (vertex.Y > top) top = vertex.Y;
+                        if (vertex.Y < bottom) bottom = vertex.Y;
+                        if (vertex.Z < close) close = vertex.Z;
+                        if (vertex.Z > distant) distant = vertex.Z;
+                    }
+                }
+
+                this.First = new Vector3(left, bottom, close);
+                this.Second = new Vector3(right, top, distant);
+            }
+        }
+    }
+}
