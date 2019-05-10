cf0c7a4a51c6c7b95168e1a00fb8a2dfd97feb08
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 00:56:55 +0200

    Created interface for debuggable entities

---
+++ DebugData/IDebuggable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.DebugData
+{
+    interface IDebuggable
+    {
+        public bool Debug { get; set; }
+    }
+}
