1c17454bb6a46b3e529b7b8f7ddd61045e019d59
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 01:00:03 +0200

    MtlLoader for ObjLoader

---
+++ ObjLoader/MtlLoader.cs
@@ -0,0 +1,68 @@
+﻿using SharpEngine_Core.SolidData;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace SharpEngine_Core.ObjLoader
+{
+    class MtlLoader
+    {
+        public static MaterialBuffer LoadMaterial(string path, string fileName)
+        {
+            MaterialBuffer materialBuffer = new MaterialBuffer();
+            Material tempMaterial = null;
+
+            List<float> materialParams = new List<float>();
+
+            string[] lines = File.ReadAllLines(path + "/" + fileName);
+
+            foreach (string line in lines)
+            {
+                string[] split = line.Split(' ');
+
+                materialParams = new List<float>();
+                for (int i = 1; i < split.Length; i++)
+                {
+                    float val;
+                    if (float.TryParse(split[i], NumberStyles.Float, CultureInfo.InvariantCulture, out val))
+                    {
+                        materialParams.Add(val);
+                    }
+                }
+
+                switch (split[0])
+                {
+                    case "newmtl":
+                        tempMaterial = new Material(split[1]);
+                        materialBuffer.Add(tempMaterial);
+                        break;
+                    case "Ka":
+                        tempMaterial.Ka = materialParams.ToArray();
+                        break;
+                    case "Kd":
+                        tempMaterial.Kd = materialParams.ToArray();
+                        break;
+                    case "Ks":
+                        tempMaterial.Ks = materialParams.ToArray();
+                        break;
+                    case "d":
+                        tempMaterial.d = materialParams.ToArray()[0];
+                        break;
+                    case "Tr":
+                        tempMaterial.d = materialParams.ToArray()[0];
+                        break;
+                    case "s":
+                        tempMaterial.d = materialParams.ToArray()[0];
+                        break;
+                    case "map_Kd":
+                        tempMaterial.textureID = TextureLoader.LoadFromPath(path + "/" + split[1].Replace(@".\", ""));
+                        break;
+                }
+            }
+
+            return materialBuffer;
+        }
+    }
+}
