71b37cf5ed2e7b317fe0427d0c26e5f8a88fe043
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 01:04:12 +0200

    Example of an anctual Solid

---
+++ Solids/Solid.cs
@@ -0,0 +1,17 @@
+﻿using OpenTK;
+using SharpEngine_Core.SolidData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.Solids
+{
+    class Solid : ASolid
+    {
+        public Solid(Face3[] faces, Vector3 pos)
+        {
+            this.Body.Faces = faces;
+            this.Pos = pos;
+        }
+    }
+}
