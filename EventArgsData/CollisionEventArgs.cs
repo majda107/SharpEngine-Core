694bbbaae8ee83b44f8ae760521cbf83c727b273
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 03:25:03 +0200

    Added eventargs for custom events

---
+++ EventArgsData/CollisionEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using SharpEngine_Core.PhysicsData;
+using SharpEngine_Core.Solids;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.EventArgsData
+{
+    class CollisionEventArgs : EventArgs
+    {
+        public ASolid Solid { get; private set; }
+        public IForce Force { get; private set; }
+        public CollisionEventArgs(ASolid solid, IForce force)
+        {
+            this.Solid = solid;
+            this.Force = force;
+        }
+    }
+}
