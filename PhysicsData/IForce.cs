f1aff1db4bc61c2065409593ce733d61ea7f1f77
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 03:25:48 +0200

    Force interface

---
+++ PhysicsData/IForce.cs
@@ -0,0 +1,13 @@
+﻿using SharpEngine_Core.Solids;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.PhysicsData
+{
+    interface IForce
+    {
+        public bool IsActive { get; set; }
+        void UpdateForce(ASolid solid);
+    }
+}
