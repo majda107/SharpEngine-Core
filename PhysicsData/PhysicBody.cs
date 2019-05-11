207d2c634ab4c60f8e59d54c8d032f438ceed788
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 03:32:08 +0200

    Implemented forces

--- PhysicsData/PhysicBody.cs
+++ PhysicsData/PhysicBody.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpEngine_Core.Solids;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,27 @@ namespace SharpEngine_Core.PhysicsData
 {
     class PhysicBody
     {
+        public RigidBody RigidBody { get; set; }
+        public List<IForce> Forces { get; private set; }
+        public ASolid Solid { get; private set; }
+
+        public PhysicBody(ASolid solid)
+        {
+            this.Forces = new List<IForce>();
+            this.Solid = solid;
+        }
+
+        public void AddForce(IForce force)
+        {
+            this.Forces.Add(force);
+        }
+
+        public void UpdateForces()
+        {
+            foreach(IForce force in this.Forces)
+            {
+                force.UpdateForce(this.Solid);
+            }
+        }
     }
 }
