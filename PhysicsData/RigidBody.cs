e40021c89bff7e875e71124e45d8d46955312174
Marian Trpkos <majda.trpkos@gmail.com>
2019-05-11 03:26:02 +0200

    Rigidbody created

---
+++ PhysicsData/RigidBody.cs
@@ -0,0 +1,25 @@
+﻿using SharpEngine_Core.Solids;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpEngine_Core.PhysicsData
+{
+    class RigidBody
+    {
+        public float Weight { get; private set; }
+        public PhysicBody PhysicBody { get; private set; }
+        public RigidBody(float weight, PhysicBody physicBody)
+        {
+            this.PhysicBody = physicBody;
+            this.Weight = weight;
+
+            this.PhysicBody.Solid.OnCollision += (o, e) =>
+            {
+                ASolid sender = o as ASolid;
+
+                if(e.Solid.PhysicBody.RigidBody != null) e.Force.IsActive = false;
+            };
+        }
+    }
+}
